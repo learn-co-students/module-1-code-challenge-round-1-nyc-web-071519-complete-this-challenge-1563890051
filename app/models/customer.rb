a24b99deb177bfc32a10910114c3c7cfaed84683
github username <github email address>
2019-07-23 10:59:34 -0400

    clone

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,60 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  # Class Methods
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Customer.all.find do |cust_inst|
+      cust_inst.full_name == name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    Customer.all.select do |cust_inst|
+      cust_inst.first_name == name
+    end
+  end
+
+  def self.all_names
+    Customer.all.map do |cust_inst|
+      cust_inst.full_name
+    end
+  end
+
+  # Instance Methods
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def cust_reviews # returns the reviews by an instance of Customer
+    Review.all.select do |review_inst|
+      review_inst.customer == self
+    end
+  end
+
+  def num_reviews
+    cust_reviews.length
+  end
+
+  def restaurants
+    cust_reviews.map do |review_inst|
+      review_inst.restaurant
+    end
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,55 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  # Class Methods
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find do |rest_inst|
+      rest_inst.name == name
+    end
+  end
+  
+  # Instance Methods
+
+  def reviews # returns the reviews of an instance of Restaurant
+    Review.all.select do |review_inst|
+      review_inst.restaurant == self
+    end
+  end
+
+  def customers
+    reviews.map do |review_inst|
+      review_inst.customer
+    end
+  end
+
+  def average_star_rating
+    rest_ratings = reviews.map do |review_inst|
+      review_inst.rating
+    end
+    sum = 0
+    rest_ratings.each do |rtg|
+      sum += rtg
+    end
+    sum / rest_ratings.length
+  end
+
+  def longest_review
+    contents = reviews.map do |review_inst|
+      review_inst.content
+    end
+    contents.max_by do |cnt|
+      cnt.length
+    end
+  end
 end
